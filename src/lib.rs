5cc276f59f4a9a8acb4be15d858efe3d3edad829
BurntPizza <xburntpizza@gmail.com>
2017-07-15 20:54:45 -0400

    Materials, lights, and shadows, oh my!

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,115 @@
 
+extern crate image;
 extern crate ord_subset;
 extern crate alga;
 extern crate ncollide as nc;
 extern crate nalgebra as na;
 
+use image::Rgb;
 use ord_subset::*;
 use alga::linear::EuclideanSpace;
 use na::{Point3, Vector3};
-use nc::query::RayCast;
 use na::geometry::Isometry3;
+use nc::query::RayCast;
 
-pub use na::zero;
+use std::any::Any;
 
-pub type U = f64;
+pub type U = f32;
 pub type P3 = Point3<U>;
 pub type V3 = Vector3<U>;
 pub type Ray = nc::query::Ray3<U>;
+pub type Color = Rgb<U>;
 
 pub struct Scene {
-    objects: Vec<Box<Object + Sync>>,
+    lights: Vec<PointLight>,
+    spheres: Vec<Sphere>,
 }
 
 impl Scene {
     pub fn new<I>(iter: I) -> Self
     where
-        I: IntoIterator<Item = Box<Object + Sync>>,
+        I: IntoIterator<Item = Box<Any>>,
     {
-        Scene { objects: iter.into_iter().collect() }
+        let mut spheres = vec![];
+        let mut lights = vec![];
+
+        for obj in iter {
+            if obj.is::<Sphere>() {
+                spheres.push(*obj.downcast().unwrap());
+                continue;
+            }
+            if obj.is::<PointLight>() {
+                lights.push(*obj.downcast().unwrap());
+                continue;
+            }
+            panic!();
+        }
+
+        Scene { spheres, lights }
     }
 
     pub fn cast_ray(&self, ray: &Ray) -> Option<Intersection> {
-        self.objects
+        self.spheres
             .iter()
             .filter_map(|obj| obj.intersection(ray))
             .ord_subset_min_by_key(|i| i.dist)
     }
+
+    pub fn lights(&self) -> &[PointLight] {
+        &*self.lights
+    }
 }
 
-pub struct Intersection {
-    // location: P3,
+pub struct PointLight {
+    pub pos: P3,
+    pub color: Color,
+}
+
+impl PointLight {
+    pub fn new(pos: P3, color: Color) -> Self {
+        PointLight { pos, color }
+    }
+}
+
+pub struct Intersection<'a> {
+    pub obj: &'a Object,
+    pub pos: P3,
     pub dist: U,
+    pub norm: V3,
 }
 
 pub trait Object {
     fn intersection(&self, ray: &Ray) -> Option<Intersection>;
+    fn material(&self) -> &Material;
 }
 
 pub struct Sphere {
-    shape: nc::shape::Ball<U>,
     pos: P3,
+    shape: nc::shape::Ball<U>,
+    material: Material,
+}
+
+#[derive(Copy, Clone)]
+pub struct Material {
+    pub color: Color,
+    pub diffuse_coef: U,
+}
+
+impl Material {
+    pub fn new(color: Color, diffuse_coef: U) -> Self {
+        Material {
+            color,
+            diffuse_coef,
+        }
+    }
 }
 
 impl Sphere {
-    pub fn new(pos: P3, radius: U) -> Self {
+    pub fn new(pos: P3, radius: U, material: Material) -> Self {
         Sphere {
-            shape: nc::shape::Ball::new(radius),
             pos,
+            shape: nc::shape::Ball::new(radius),
+            material,
         }
     }
 }
@@ -63,8 +117,18 @@ impl Sphere {
 impl Object for Sphere {
     fn intersection(&self, ray: &Ray) -> Option<Intersection> {
         let m = Isometry3::new(self.pos.coordinates(), na::zero());
-        self.shape.toi_with_ray(&m, ray, true).map(|toi| {
-            Intersection { dist: toi }
+        self.shape.toi_and_normal_with_ray(&m, ray, true).map(|i| {
+            let pos = ray.origin + ray.dir * i.toi;
+            Intersection {
+                pos,
+                dist: i.toi,
+                obj: self as &Object,
+                norm: i.normal,
+            }
         })
+    }
+
+    fn material(&self) -> &Material {
+        &self.material
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 
+extern crate nalgebra as na;
+extern crate rayon;
 extern crate image;
 extern crate glimmer;
-extern crate rayon;
 
-use std::cmp::*;
-use image::{Rgb, RgbImage};
+use std::any::Any;
+
+use na::*;
 use rayon::prelude::*;
+use image::{Rgb, RgbImage, Pixel};
 use glimmer::*;
 
-const PARALLEL: bool = true;
-
 fn main() {
-    let w = 1920;
-    let h = 1080;
+    let w = 1280;
+    let h = 720;
 
-    let a = Sphere::new(P3::new(-1.5, 0.0, 1.0), 1.0);
-    let b = Sphere::new(P3::new(0.0, 0.0, 3.0), 1.0);
-    let c = Sphere::new(P3::new(1.5, 0.0, 5.0), 1.0);
-    let d = Sphere::new(P3::new(3.0, 0.0, 7.0), 1.0);
+    let c1 = Rgb { data: [0.7, 0.0, 0.0] };
+    let c2 = Rgb { data: [0.2, 0.6, 0.2] };
+    let c3 = Rgb { data: [0.9, 0.9, 0.9] };
 
-    let scene = Scene::new(vec![a, b, c, d].into_iter().map(|o| {
-        Box::new(o) as Box<Object + Sync>
-    }));
+    let m1 = Material::new(c1, 0.8);
+    let m2 = Material::new(c2, 0.8);
+    let m3 = Material::new(c3, 0.8);
+
+    let a = Sphere::new(P3::new(-1.5, 0.0, 1.0), 1.0, m1);
+    let b = Sphere::new(P3::new(0.0, 0.0, 3.0), 1.0, m2);
+    let c = Sphere::new(P3::new(1.5, 0.0, 5.0), 1.0, m3);
+    let d = Sphere::new(P3::new(3.0, 0.0, 7.0), 1.0, m1);
+
+    let l1 = PointLight::new(P3::new(-2.0, -2.0, -1.0), c3);
+    let l2 = PointLight::new(P3::new(0.0, 5.0, -2.0), c3.map(|c| c * 0.5));
+
+    let scene = Scene::new(vec![x(a), x(b), x(c), x(d), x(l1), x(l2)]);
 
     let mut image = RgbImage::new(w, h);
 
@@ -35,33 +45,54 @@ fn render(scene: &Scene, image: &mut RgbImage) {
     let h = image.height();
     let t = min(w, h) as U;
 
-    if PARALLEL {
-        (0..h).into_par_iter().for_each(|y| for x in 0..w {
-            process(x, y, &image, &scene, w, h, t);
-        });
-    } else {
-        for y in 0..h {
-            for x in 0..w {
-                process(x, y, &image, &scene, w, h, t);
+    (0..h).into_par_iter().for_each(|y| for x in 0..w {
+        let xt = (x as U - w as U * 0.5) / t;
+        let yt = (y as U - h as U * 0.5) / t;
+        let ray = Ray::new(P3::new(0.0, 0.0, 0.0), V3::new(xt, yt, 1.0).normalize());
+
+        let c = scene.cast_ray(&ray).map_or(Rgb { data: [0, 0, 0] }, |it| {
+            // ambient
+            let ambient = 0.05;
+            let mut diffuse = [0.0; 3];
+            let m = it.obj.material();
+
+            for light in scene.lights() {
+                let lv = (light.pos - it.pos).normalize();
+                let ldist2 = distance_squared(&light.pos, &it.pos);
+                // lv * 0.0001 is a hack to avoid this shadow ray colliiding with the current object
+                let lray = Ray::new(it.pos + lv * 0.0001, lv);
+
+                if let Some(lit) = scene.cast_ray(&lray) {
+                    if lit.dist * lit.dist <= ldist2 {
+                        continue;
+                    }
+                }
+
+                let ldot = dot(&lv, &it.norm);
+
+                for i in 0..3 {
+                    diffuse[i] += ldot * m.color[i] * light.color[i];
+                }
             }
+
+            let mut data = [0; 3];
+
+            for i in 0..3 {
+                let c = ((m.color[i] * ambient + diffuse[i]) * 255.0) as u32;
+                data[i] = min(c, 255) as u8;
+            }
+
+            Rgb { data }
+        });
+
+        // can't share image mutably between threads, so here's a hacked put_pixel:
+        // safe since all (x, y)s are never repeated
+        unsafe {
+            *(image.get_pixel(x, y) as *const _ as *mut _) = c;
         }
-    }
+    })
 }
 
-#[inline]
-fn process(x: u32, y: u32, image: &RgbImage, scene: &Scene, w: u32, h: u32, t: U) {
-    let xt = (x as U - w as U * 0.5) / t;
-    let yt = (y as U - h as U * 0.5) / t;
-    let ray = Ray::new(P3::new(0.0, 0.0, 0.0), V3::new(xt, yt, 1.0).normalize());
-    let d = scene.cast_ray(&ray).map_or(
-        0,
-        |it| (255.0 - it.dist * 10.0) as u8,
-    );
-    let p = Rgb { data: [d, d, d] };
-
-    // can't share image mutably between threads, so here's a hacked put_pixel:
-    // safe since all (x, y)s are never repeated
-    unsafe {
-        *(image.get_pixel(x, y) as *const _ as *mut _) = p;
-    }
+fn x<T: 'static>(t: T) -> Box<Any> {
+    Box::new(t) as Box<Any>
 }
