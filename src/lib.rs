29ef802e11dc407231667fcc8ae4031a8f6b9a5e
BurntPizza <xburntpizza@gmail.com>
2017-07-16 20:04:00 -0400

    Textures and reflections!

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,18 @@ extern crate alga;
 extern crate ncollide as nc;
 extern crate nalgebra as na;
 
+use std::sync::Arc;
+
 use image::Rgb;
 use ord_subset::*;
 use alga::linear::EuclideanSpace;
-use na::{Point3, Vector3};
-use na::geometry::Isometry3;
+use na::{Point2, Point3, Vector2, Vector3};
 use nc::query::RayCast;
 
 pub type U = f32;
+pub type P2 = Point2<U>;
 pub type P3 = Point3<U>;
+pub type V2 = Vector2<U>;
 pub type V3 = Vector3<U>;
 pub type Ray = nc::query::Ray3<U>;
 pub type Color = Rgb<U>;
@@ -62,64 +65,135 @@ impl PointLight {
     }
 }
 
+pub enum ObjRef<'a> {
+    Sphere(&'a Sphere),
+}
+
 pub struct Intersection<'a> {
-    pub obj: &'a Object,
+    pub info: &'a ObjInfo,
     pub pos: P3,
     pub dist: U,
     pub norm: V3,
+    pub uv: P2,
 }
 
 pub trait Object {
     fn intersection(&self, ray: &Ray) -> Option<Intersection>;
-    fn material(&self) -> &Material;
+    fn info(&self) -> &ObjInfo;
+}
+
+pub struct ObjInfo {
+    pub material: Material,
+}
+
+// Solid color | Procedural texture
+// reflectivity
+// transparency
+// index of refraction
+// emissivity
+
+#[derive(Copy, Clone)]
+pub struct Material {
+    pub texture: Texture,
+    pub diffuse_coef: U,
+    pub reflectivity: U,
+}
+
+
+impl ObjInfo {
+    pub fn new(material: Material) -> Self {
+        Self { material }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Texture {
+    Solid(Color),
+    Procedural(fn(P2) -> Color),
+}
+
+impl Texture {
+    pub fn color(&self, p: P2) -> Color {
+        match *self {
+            Texture::Solid(c) => c,
+            Texture::Procedural(f) => f(p),
+        }
+    }
+}
+
+pub const XOR_TEXTURE: Texture = Texture::Procedural(xor_texture_impl);
+
+pub fn solid_texture(c: Color) -> Texture {
+    Texture::Solid(c)
+}
+
+fn xor_texture_impl(p: P2) -> Color {
+    let s = 128.0;
+    let p = p * s;
+    let x = p[0] as u16;
+    let y = p[1] as u16;
+    let c = (x ^ y) as U / s;
+    Color { data: [c; 3] }
 }
 
 pub struct Sphere {
     pos: P3,
     shape: nc::shape::Ball<U>,
-    material: Material,
+    info: Arc<ObjInfo>,
 }
-
-#[derive(Copy, Clone)]
-pub struct Material {
-    pub color: Color,
-    pub diffuse_coef: U,
-}
-
 impl Material {
-    pub fn new(color: Color, diffuse_coef: U) -> Self {
+    pub fn new(texture: Texture, diffuse_coef: U, reflectivity: U) -> Self {
         Material {
-            color,
+            texture,
             diffuse_coef,
+            reflectivity,
         }
     }
 }
 
 impl Sphere {
-    pub fn new(pos: P3, radius: U, material: Material) -> Self {
+    pub fn new(pos: P3, radius: U, info: Arc<ObjInfo>) -> Self {
         Sphere {
             pos,
             shape: nc::shape::Ball::new(radius),
-            material,
+            info,
         }
     }
 }
 
 impl Object for Sphere {
     fn intersection(&self, ray: &Ray) -> Option<Intersection> {
-        let m = Isometry3::new(self.pos.coordinates(), na::zero());
-        self.shape.toi_and_normal_with_ray(&m, ray, true).map(|i| {
-            let pos = ray.origin + ray.dir * i.toi;
-            Intersection {
-                pos,
-                dist: i.toi,
-                obj: self as &Object,
-                norm: i.normal,
-            }
-        })
+        let m = na::Translation3::from_vector(self.pos.coordinates());
+        self.shape
+            .toi_and_normal_and_uv_with_ray(&m, ray, true)
+            .map(|i| {
+                let pos = ray.origin + ray.dir * i.toi;
+                Intersection {
+                    pos,
+                    dist: i.toi,
+                    info: self.info(),
+                    norm: i.normal,
+                    uv: i.uvs.unwrap(),
+                }
+            })
     }
 
-    fn material(&self) -> &Material {
-        &self.material
+    fn info(&self) -> &ObjInfo {
+        &self.info
+    }
+}
+
+
+impl<'a> Object for ObjRef<'a> {
+    fn intersection(&self, ray: &Ray) -> Option<Intersection> {
+        match *self {
+            ObjRef::Sphere(ref s) => s.intersection(ray),
+        }
+    }
+
+    fn info(&self) -> &ObjInfo {
+        match *self {
+            ObjRef::Sphere(ref s) => s.info(),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate rayon;
 extern crate image;
 extern crate glimmer;
 
+use std::sync::Arc;
+
 use na::*;
 use rayon::prelude::*;
 use image::{Rgb, RgbImage, Pixel};
@@ -17,17 +19,24 @@ fn main() {
     let c2 = Rgb { data: [0.2, 0.6, 0.2] };
     let c3 = Rgb { data: [0.9, 0.9, 0.9] };
 
-    let m1 = Material::new(c1, 0.8);
-    let m2 = Material::new(c2, 0.8);
-    let m3 = Material::new(c3, 0.8);
 
-    let a = Sphere::new(P3::new(-1.5, 0.0, 1.0), 1.0, m1);
-    let b = Sphere::new(P3::new(0.0, 0.0, 3.0), 1.0, m2);
-    let c = Sphere::new(P3::new(1.5, 0.0, 5.0), 1.0, m3);
-    let d = Sphere::new(P3::new(3.0, 0.0, 7.0), 1.0, m1);
+    let m1 = Material::new(solid_texture(c1), 0.8, 0.0);
+    let m2 = Material::new(solid_texture(c2), 0.8, 0.0);
+    let m3 = Material::new(XOR_TEXTURE, 0.8, 0.0);
+    let m4 = Material::new(solid_texture(c3), 0.2, 0.8);
+
+    let info1 = Arc::new(ObjInfo::new(m1));
+    let info2 = Arc::new(ObjInfo::new(m2));
+    let info3 = Arc::new(ObjInfo::new(m3));
+    let info4 = Arc::new(ObjInfo::new(m4));
+
+    let a = Sphere::new(P3::new(-1.5, 0.0, 1.0), 1.0, info1.clone());
+    let b = Sphere::new(P3::new(0.0, 0.0, 3.0), 1.0, info2.clone());
+    let c = Sphere::new(P3::new(1.5, 0.0, 5.0), 1.0, info3.clone());
+    let d = Sphere::new(P3::new(3.0, 0.0, 3.0), 1.0, info4.clone());
 
     let l1 = PointLight::new(P3::new(-2.0, -2.0, -1.0), c3);
-    let l2 = PointLight::new(P3::new(0.0, 5.0, -2.0), c3.map(|c| c * 0.5));
+    let l2 = PointLight::new(P3::new(0.0, 5.0, -2.0), c3.map(|c| c * 1.0));
 
     let mut scene = Scene::new();
     scene.add(a);
@@ -37,7 +46,24 @@ fn main() {
     scene.add_light(l1);
     scene.add_light(l2);
 
-    let mut image = RgbImage::new(w, h);
+    let infos = [info1, info2, info3, info4];
+    let n = 8;
+    let r = 1.0;
+    for i in 0..n {
+        let x = ((i * 3562215327) ^ 463557371) + 3;
+        let a = i as U / n as U + 0.03;
+        scene.add(Sphere::new(
+            P3::new(
+                (a * 3.14).cos() * r,
+                0.5,
+                3.0 - (a * 3.14).sin() * r,
+            ),
+            0.1,
+            infos[x % infos.len()].clone(),
+        ));
+    }
+
+    let mut image = RgbImage::from_fn(w, h, |_, _| Rgb { data: [0; 3] });
 
     render(&scene, &mut image);
 
@@ -49,50 +75,72 @@ fn render(scene: &Scene, image: &mut RgbImage) {
     let h = image.height();
     let t = min(w, h) as U;
 
-    (0..h).into_par_iter().for_each(|y| for x in 0..w {
-        let xt = (x as U - w as U * 0.5) / t;
-        let yt = (y as U - h as U * 0.5) / t;
-        let ray = Ray::new(P3::new(0.0, 0.0, 0.0), V3::new(xt, yt, 1.0).normalize());
+    (0..h).into_par_iter().for_each(|y| {
+        for x in 0..w {
+            let xt = (x as U - w as U * 0.5) / t;
+            let yt = (y as U - h as U * 0.5) / t;
+            let ray = Ray::new(P3::new(0.0, 0.0, 0.0), V3::new(xt, yt, 1.0).normalize());
 
-        let c = scene.cast_ray(&ray).map_or(Rgb { data: [0, 0, 0] }, |it| {
-            // ambient
-            let ambient = 0.05;
-            let mut diffuse = [0.0; 3];
-            let m = it.obj.material();
-
-            for light in scene.lights() {
-                let lv = (light.pos - it.pos).normalize();
-                let ldist2 = distance_squared(&light.pos, &it.pos);
-                // lv * 0.0001 is a hack to avoid this shadow ray colliiding with the current object
-                let lray = Ray::new(it.pos + lv * 0.0001, lv);
-
-                if let Some(lit) = scene.cast_ray(&lray) {
-                    if lit.dist * lit.dist <= ldist2 {
-                        continue;
-                    }
-                }
-
-                let ldot = dot(&lv, &it.norm);
-
-                for i in 0..3 {
-                    diffuse[i] += ldot * m.color[i] * light.color[i];
-                }
-            }
-
+            let c = cast(&scene, &ray, 0);
             let mut data = [0; 3];
 
             for i in 0..3 {
-                let c = ((m.color[i] * ambient + diffuse[i]) * 255.0) as u32;
-                data[i] = min(c, 255) as u8;
+                data[i] = min((c[i] as U * 255.0) as u8, 255);
             }
 
-            Rgb { data }
-        });
-
-        // can't share image mutably between threads, so here's a hacked put_pixel:
-        // safe since all (x, y)s are never repeated
-        unsafe {
-            *(image.get_pixel(x, y) as *const _ as *mut _) = c;
+            // can't share image mutably between threads, so here's a hacked put_pixel:
+            // safe since all (x, y)s are never repeated
+            unsafe {
+                *(image.get_pixel(x, y) as *const _ as *mut _) = Rgb { data };
+            }
         }
+    })
+}
+
+fn cast(scene: &Scene, ray: &Ray, depth: u8) -> Color {
+    scene.cast_ray(&ray).map_or(Rgb { data: [0.0; 3] }, |it| {
+        let ambient = 0.05;
+        let mut diffuse = [0.0; 3];
+        let m = it.info.material;
+        let diffuse_coef = m.diffuse_coef;
+        let tcolor = m.texture.color(it.uv);
+
+        for light in scene.lights() {
+            let lv = (light.pos - it.pos).normalize();
+            let ldist2 = distance_squared(&light.pos, &it.pos);
+            // lv * 0.0001 is a hack to avoid shadow ray colliding with the current object
+            let lray = Ray::new(it.pos + lv * 0.0001, lv);
+
+            if let Some(lit) = scene.cast_ray(&lray) {
+                if lit.dist * lit.dist <= ldist2 {
+                    continue;
+                }
+            }
+
+            let ldot = dot(&lv, &it.norm);
+
+            for i in 0..3 {
+                diffuse[i] += ldot * tcolor[i] * diffuse_coef * light.color[i];
+            }
+        }
+
+        if depth < 3 && m.reflectivity != 0.0 {
+            let v = ray.origin - it.pos;
+            let r = (2.0 * dot(&it.norm, &v) * it.norm - v).normalize();
+            let ray = Ray::new(it.pos + r * 0.0001, r);
+            let c = cast(scene, &ray, depth + 1);
+
+            for i in 0..3 {
+                diffuse[i] += c[i] * m.reflectivity;
+            }
+        }
+
+        let mut data = [0.0; 3];
+
+        for i in 0..3 {
+            data[i] = tcolor[i] * ambient + diffuse[i];
+        }
+
+        Rgb { data }
     })
 }
